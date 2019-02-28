031ec25626ad39b762852b323efa50b66eb532fb
Udit Namdev <unamdev0@gmail.com>
2019-03-01 02:26:08 +0530

    Added remaining posts route and made join and leave view in group

--- group/models.py
+++ group/models.py
@@ -16,8 +16,8 @@ register = template.Library()
 class Group(models.Model):
 	name = models.CharField(max_length=29,unique=True)
 	slug = models.SlugField(allow_unicode=True,unique=True)
-	description = models.TextField(black=True,default=' ')
-	description_html = models.TextField(editable=False,default=" ",black=True)
+	description = models.TextField(blank=True,default=' ')
+	description_html = models.TextField(editable=False,default=" ",blank=True)
 	members = models.ManyToManyField(User,through='GroupMember')
 
 
@@ -36,8 +36,8 @@ class Group(models.Model):
 
 
 class GroupMember(models.Model):
-	group = models.ForeignKey(Group,related_name='memberships')
-	user = models.ForeignKey(User,related_name='user_groups')
+	group = models.ForeignKey(Group,related_name='memberships',on_delete=model.CASCADE)
+	user = models.ForeignKey(User,related_name='user_groups',on_delete=model.CASCADE)
 
 	def __str__(self):
 		return self.user.username
--- group/views.py
+++ group/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 
 from django.contrib.auth.mixins import LoginRequiredMixin,PermissionReuqiredMixin
-
+from django.contrib import messages
 from django.urls import reverse
 
 from django.views import generic
-
+from django.shortcuts import get_object_or_404
 from group.models import Group
 
 # Create your views here.
 
-
+ 
 class CreateGroup(LoginRequiredMixin,generic.CreateView):
 	fields = ('name','description')
 	model = Group
@@ -21,3 +21,40 @@ class SingleGroup(generic.DetailView):
 
 class ListGroups(generic.ListView):
 	model = Group
+
+class JoinGroup(LoginRequiredMixin,generic.RedirectView):
+
+	def get_redirect_url(self,*args,**kwargs):
+		return revese ('groups:single',kwargs={'slug':self.kwargs.get('slug')})
+
+	def get(self,request,*args,**kwargs):
+		group = get_object_or_404(Group,slug=self.kwargs.get('slug'))
+
+		try:
+			GroupMember.objects.create(user=self.request.user,group=group)
+
+		except IntegrityError:
+			messages.warning(self.request,("You are already a member"))
+		else:
+			messages.success(self.request,"You are now a member")
+
+		return super().get(request,*args,**kwargs)
+
+class LeaveGroup(LoginRequiredMixin,generic.RedirectView):
+
+	def get_redirect_url(self,*args,**kwargs):
+		return revese ('groups:single',kwargs={'slug':self.kwargs.get('slug')})
+
+	def get(self,request,*args,**kwargs):
+
+		try:
+			membership = models.GroupMember.objects.filter(
+				user = self.request.user,
+				group__slug = self.kwargs,get('slug')).get()
+
+		except:
+			message.warning(self.request,"You are not part of this group")
+		else:
+			membership.delete()
+			message.success(self.request,"You have left this group")
+		return super().get(request,*args,**kwargs)
